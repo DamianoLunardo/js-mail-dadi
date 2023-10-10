7d54251d9bdc14956abf81af542aa0066155dd07
DamianoL <damianolunardo@hotmail.it>
2023-10-10 09:11:47 +0200

    Add 2nd version for exercise 1 with for

--- main.js
+++ main.js
@@ -1,32 +1,72 @@
 console.log("Hello World");
 
  // Riferimento alla lista delle email autorizzate
- const listaAutorizzati = ['utente1@example.com', 'utente2@example.com', 'utente3@example.com', 'utente4@example.com', 'utente5@example.com'];
+ //const listaAutorizzati = ['utente1@example.com', 'utente2@example.com', 'utente3@example.com', 'utente4@example.com', 'utente5@example.com'];
 
  // Riferimento al form e all'elemento risultato
- const emailForm = document.getElementById('emailForm');
- const risultato = document.getElementById('risultato');
- const emailInput = document.getElementById('email');
- const cancellaButton = document.getElementById('cancella');
+ //const emailForm = document.getElementById('emailForm');
+ //const risultato = document.getElementById('risultato');
+ //const emailInput = document.getElementById('email');
+ //const cancellaButton = document.getElementById('cancella');
 
  // Aggiungo un gestore di eventi al modulo di invio
- emailForm.addEventListener('submit', function(event) {
-     event.preventDefault(); 
-     const emailUtente = document.getElementById('email').value;
+ //emailForm.addEventListener('submit', function(event) {
+  //   event.preventDefault(); 
+   //  const emailUtente = document.getElementById('email').value;
 
      // Verifico se l'email dell'utente è nella lista
-     if (listaAutorizzati.includes(emailUtente)) {
-         risultato.textContent = 'Accesso consentito. Benvenuto!';
-     } else {
-         risultato.textContent = 'Accesso negato. La tua email non è autorizzata.';
-     }
- });
+   //  if (listaAutorizzati.includes(emailUtente)) {
+   //      risultato.textContent = 'Accesso consentito. Benvenuto!';
+    // } else {
+    //     risultato.textContent = 'Accesso negato. La tua email non è autorizzata.';
+    // }
+// });
 
  // extra Aggiungo un gestore di eventi per il pulsante "Cancella"
-        cancellaButton.addEventListener('click', function() {
-            emailInput.value = ''; 
-            risultato.textContent = '';
-        });
+     //   cancellaButton.addEventListener('click', function() {
+      //      emailInput.value = ''; 
+        //    risultato.textContent = '';
+      //  });
+
+
+
+
+        // Riferimento alla lista delle email autorizzate
+const listaAutorizzati = ['utente1@example.com', 'utente2@example.com', 'utente3@example.com', 'utente4@example.com', 'utente5@example.com'];
+
+// Riferimento al form e all'elemento risultato
+const emailForm = document.getElementById('emailForm');
+const risultato = document.getElementById('risultato');
+const emailInput = document.getElementById('email');
+const cancellaButton = document.getElementById('cancella');
+
+// Aggiungo un gestore di eventi al modulo di invio
+emailForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const emailUtente = document.getElementById('email').value;
+
+    // Verifico se l'email dell'utente è nella lista usando un ciclo for
+    let accessoConsentito = false;
+    for (let i = 0; i < listaAutorizzati.length; i++) {
+        if (listaAutorizzati[i] === emailUtente) {
+            accessoConsentito = true;
+            break; // Esci dal ciclo se trovi una corrispondenza
+        }
+    }
+
+    if (accessoConsentito) {
+        risultato.textContent = 'Accesso consentito. Benvenuto!';
+    } else {
+        risultato.textContent = 'Accesso negato. La tua email non è autorizzata.';
+    }
+});
+
+// Aggiungo un gestore di eventi per il pulsante "Cancella"
+cancellaButton.addEventListener('click', function() {
+    emailInput.value = '';
+    risultato.textContent = '';
+});
+
 
 // esercizio 2
 
